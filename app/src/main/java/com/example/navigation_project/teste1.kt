6d169a7e90a202f900cf8e96b42a0df1e857f23e
Caiango <cvff12345@gmail.com>
2020-06-02 19:21:04 -0300

    Adicionei botão num fragment...

---
+++ app/src/main/java/com/example/navigation_project/teste1.kt
@@ -0,0 +1,12 @@
+package com.example.navigation_project
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class teste1 : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_teste1)
+    }
+}
