58325fb5f6cd87a081b77614a50a84f1865c2139
Caiango <cvff12345@gmail.com>
2020-06-17 23:02:33 -0300

    Implementando Fragment de transição

--- app/src/main/java/com/example/navigation_project/teste1.kt
+++ app/src/main/java/com/example/navigation_project/teste1.kt
@@ -2,17 +2,30 @@ package com.example.navigation_project
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
+import android.view.View
+import android.widget.*
 import kotlinx.android.synthetic.main.activity_teste1.*
 
 class teste1 : AppCompatActivity() {
 
+    lateinit var list: ListView
+    lateinit var text: TextView
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_teste1)
 
+        list = findViewById(R.id.lista)
+        text = findViewById(R.id.Clicked)
+
+        var nomes_lista = arrayListOf<String>("Caio", "Camila", "PPS")
+        list.adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, nomes_lista)
+        list.setOnItemClickListener{
+            parent: AdapterView<*>?, view: View?, position: Int, id: Long ->
+            Clicked.text = nomes_lista[position]
+            
+        }
 
 
         buttonChange.setOnClickListener {
@@ -25,6 +38,7 @@ class teste1 : AppCompatActivity() {
 
         if(BlankFragment.mostrar.isNotEmpty()){
             Toast.makeText(applicationContext, BlankFragment.mostrar,Toast.LENGTH_SHORT).show()
+
         }
 
     }
